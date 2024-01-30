baaa7da5c080e730d633c201ad012b92ff5b3dac
Rahiel Rodriguez <rahielrodriguez@hotmail.com>
2024-01-30 08:54:15 -0700

    Loop and try cath options added for error solutions.

--- MultiplicationTable.vb
+++ MultiplicationTable.vb
@@ -8,28 +8,39 @@ Module MultiplicationTable
         Dim currentString As String
         Dim userInput As String
         Dim selectedNumber As Integer
+        Dim leave As Boolean = False
 
-        Console.WriteLine("Please select a number for your multiplication table.")
-        userInput = Console.ReadLine()
+        Do
+            Console.WriteLine("Please select a number for your multiplication table.")
+            userInput = Console.ReadLine()
+            Try
+                selectedNumber = CInt(userInput)
 
-        selectedNumber = CInt(userInput)
+                Console.WriteLine($"Enjoy your {userInput} x {userInput} multiplication table:")
+                Console.WriteLine()
 
-            Console.WriteLine($"Enjoy your {userInput} x {userInput} multiplication table:")
-            Console.WriteLine()
+                For row = 1 To CInt(userInput)
+                    For column = 1 To CInt(userInput)
+                        currentValue = row * column
+                        currentString = CStr(currentValue)
+                        If Len(currentString) < 2 Then
+                            currentString = "0" & currentString
+                        End If
+                        currentString = currentString.PadLeft(6)
+                        Console.Write(currentString)
+                    Next
+                    Console.WriteLine()
+                    Console.WriteLine(StrDup(66, " "))
+                Next
 
-        For row = 1 To CInt(userInput)
-            For column = 1 To CInt(userInput)
-                currentValue = row * column
-                currentString = CStr(currentValue)
-                If Len(currentString) < 2 Then
-                    currentString = "0" & currentString
-                End If
-                currentString = currentString.PadLeft(6)
-                Console.Write(currentString)
-            Next
-            Console.WriteLine()
-            Console.WriteLine(StrDup(66, " "))
-        Next
+                leave = True
+
+            Catch ex As Exception
+                Console.WriteLine("Please select a whole number.")
+
+            End Try
+
+        Loop Until leave = True
 
         Console.Read()
 
